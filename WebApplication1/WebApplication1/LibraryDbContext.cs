36e7d251cc4e873e1e47019c8c00053d6669a351
zendysalgado738-bit <zendysalgado738@gmail.com>
2026-07-30 20:55:31 -0600

    Definiendo contexto de base de datos

---
+++ WebApplication1/WebApplication1/LibraryDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options): base(options) { }
+
+        public DbSet<Autor> Autores => Set<Autor>();
+
+        public DbSet<Libro> Libros => Set<Libro>();
+    }
+}
