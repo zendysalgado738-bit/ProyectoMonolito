85515198415fe69f1b9382ea8ab5f7d338efb7b4
zendysalgado738-bit <zendysalgado738@gmail.com>
2026-07-30 20:36:01 -0600

    Creando modelos de autor y libro

---
+++ WebApplication1/WebApplication1/Models/Autor.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Autor
+    {
+        public int Id { get; set; } 
+
+        public string Nombre { get; set; } = string.Empty;
+
+        public string Nacionalidad { get; set; } = string.Empty;
+
+        public List<Libro> Libros { get; set; } = new();
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Libro.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+
+        public int AnioPublicacion { get; set; } 
+    
+        public int AutorId { get; set; }
+
+        public Autor? Autor { get; set; }
+    }
+}
