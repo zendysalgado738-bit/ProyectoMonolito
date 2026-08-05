1e8d7ede2a2a212a222d8bfa356f893612781fa1
zendysalgado738-bit <zendysalgado738@gmail.com>
2026-08-04 20:52:26 -0600

    Agregamos el contexto de conexion y creacion de base de datos

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("default")
+?? "Data Source=biblioteca.db"));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +15,16 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//aplicar migraciones automaticamente
+using (var scope = app.Services.CreateScope())
+{
+
+    var db = scope.ServiceProvider.GetRequiredService<LibraryDbContext>();
+    db.Database.Migrate();
+}
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
